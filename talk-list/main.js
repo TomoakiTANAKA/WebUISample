fb9767818547f074add1edc389207a37fa271398
TomoakiTANAKA <tanaka1987@gmail.com>
2018-06-08 23:59:31 +0900

    全体を広げられるように

--- talk-list/main.js
+++ talk-list/main.js
@@ -1,6 +1,23 @@
 "use strict";
 
 $(document).ready(function () {
+  $('#btn1').on('click', function() {
+    // TODO：本当はセレクトボックスの中身を取得して「1」とか「2」とか取得する
+    var index = 0;
+
+    var targetObject = $('.row.cardlist-area__row').eq(index);
+
+    targetObject.find('.message-card__wrap').each(function() {
+      var height = $(this).find("p").height();
+      $(this).find(".message-card__content").animate({
+        "height": height
+      }, "slow");
+      $(this).addClass("active");
+    });
+  });
+
+
+
   $('.message-card__wrap').on('click', function () {
     if (!$(this).hasClass("active")) {
       // クリックされたらコンテナの高さを固定値から、文字の高さに変更する
